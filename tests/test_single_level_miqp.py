738b483fd8f459ab35d6294d0091f15c8e7b07f7
demirayonur <onur.demiray@epfl.ch>
2022-06-13 01:11:21 +0300

    test single-level MIQP

---
+++ tests/test_single_level_miqp.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.insert(1, './src')
+
+import random_instance
+import single_level_miqp
+
+n_node, n_arc, n_com, my_seed = 24,96,10,1234
+instance = random_instance.Instance(n_node, n_arc, n_com, my_seed)
+formulation = single_level_miqp.SingleLevelMIQP(instance)
+formulation.build_model()
+formulation.write_model()
+formulation.solve_model()
+formulation.summarize_results()
+
+print('done')
+
